ec87e22093f4e89809562afa1c939116a5d7bfab
pandula <pandula@LAPTOP-HSQ6BQE7.localdomain>
2022-03-26 20:36:43 +1030

    task 3-2

--- task3.js
+++ task3.js
@@ -1,6 +1,33 @@
 var counter = 0;
 
 function count() {
-    document.getElementById("mcount").innerText = counter;
     counter++;
+    document.getElementById('mcount').innerText = counter;
 }
+
+function new_post() {
+    let date = new Date().toLocaleString();
+    let content = document.getElementById('message').value;
+    let post = document.getElementById('posts');
+    
+    add_post(date, content, post);
+}
+
+function add_post(date,content, post) {
+    let postDate = document.createElement("p");
+    let postContent = document.createElement("p");
+    postDate.class = 'post-date';
+    postDate.style.fontWeight = "bold";
+    postDate.style.color = "grey";
+    postContent.class = 'post-content';
+    let textDate = document.createTextNode(date);
+    let textContent = document.createTextNode(content);
+    postDate.appendChild(textDate);
+    postContent.appendChild(textContent);
+    post.appendChild(postDate);
+    post.appendChild(postContent);
+}
+
+
+
+
